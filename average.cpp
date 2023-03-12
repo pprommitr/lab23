5b4f0831eba781bb5916a972594a3ed6af53f0a3
pinnaree <pprommitr@gmail.com>
2023-03-12 23:32:24 +0700

    lab 23 average okk

--- average.cpp
+++ average.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    if(argc == 1){
+        cout << "Please input numbers to find average."<< endl;
+       
+    }
+    else{
+        cout << "---------------------------------\n";
+        cout <<"Average of "<< argc-1 <<" numbers = ";
+        float sum = 0;
+        for(int i=1; i<argc; i++){
+            sum += atof(argv[i]);
+        }
+        cout << sum/(argc-1) << endl;
+        cout << "---------------------------------\n";
+    }
+    return 0;
+}
 
